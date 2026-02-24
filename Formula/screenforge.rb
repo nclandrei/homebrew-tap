eef2b55dbbaeed17ebf097195de4c40afbeaacb9
Andrei-Mihai Nicolae <andrei@nicolaeandrei.com>
2026-02-24 18:09:47 +0200

    Update screenforge SHA256

--- Formula/screenforge.rb
+++ Formula/screenforge.rb
@@ -2,7 +2,7 @@ class Screenforge < Formula
   desc "CLI tool for generating App Store-style marketing screenshots"
   homepage "https://github.com/nclandrei/screenforge"
   url "https://github.com/nclandrei/screenforge/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "bd80081f7f5fd6d83054403b03ef2462f14d094b64feceeac74defd259ab5ec1"
   license "MIT"
 
   depends_on "rust" => :build
