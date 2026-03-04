e987d6e47c5bffce7f4d14cfbcb2adb7472dd4a0
Andrei-Mihai Nicolae <andrei@nicolaeandrei.com>
2026-03-04 15:34:43 +0200

    screenforge 0.1.4

--- Formula/screenforge.rb
+++ Formula/screenforge.rb
@@ -1,8 +1,8 @@
 class Screenforge < Formula
   desc "CLI tool for generating App Store-style marketing screenshots"
   homepage "https://github.com/nclandrei/screenforge"
-  url "https://github.com/nclandrei/screenforge/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "2fa0a20dffb788710b518da924952a671c009b776dec4c443aae2fd42c568680"
+  url "https://github.com/nclandrei/screenforge/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "da44291dfc6e46b2163412acb8e009f3c474480d8145923fc573f6f5a3e6671b"
   license "MIT"
 
   depends_on "rust" => :build
