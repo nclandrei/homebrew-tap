bd1ee2fd3faffc1f3cdcc1e1d1272dae6a741bae
Andrei-Mihai Nicolae <andrei@nicolaeandrei.com>
2026-03-14 12:35:35 +0200

    planwarden 0.1.0 checksums

--- Formula/planwarden.rb
+++ Formula/planwarden.rb
@@ -5,14 +5,17 @@ class Planwarden < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/nclandrei/planwarden/releases/download/v0.1.0/planwarden-aarch64-apple-darwin.tar.xz"
+      sha256 "f454f4f263dca7dcb5e4fcd6754b45616496741e52cb6f56455fe56a2769e93d"
     end
     if Hardware::CPU.intel?
       url "https://github.com/nclandrei/planwarden/releases/download/v0.1.0/planwarden-x86_64-apple-darwin.tar.xz"
+      sha256 "ca68bcabdc01e2b4ec592d2ca418681230e73d7b949ca63afa40b46ad237a96b"
     end
   end
   if OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/nclandrei/planwarden/releases/download/v0.1.0/planwarden-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "36088ab39c6b572f9011f3c1f4b12a203895a118aefe373649857a86efb00ca8"
     end
   end
   license "MIT"
