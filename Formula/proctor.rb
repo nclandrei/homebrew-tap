c92f48d042646f3636204fd22b7ef746a960f5c6
Andrei-Mihai Nicolae <andrei@nicolaeandrei.com>
2026-03-16 11:00:35 +0200

    proctor 0.1.1

--- Formula/proctor.rb
+++ Formula/proctor.rb
@@ -1,21 +1,22 @@
 class Proctor < Formula
   desc "Manual verification contract CLI for coding agents"
   homepage "https://github.com/nclandrei/proctor"
-  version "0.1.0"
+  version "0.1.1"
+
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/nclandrei/homebrew-tap/releases/download/proctor-v0.1.0/proctor-aarch64-apple-darwin.tar.gz"
-      sha256 "f111bfaba251ecddf5884b732f9cd901d44d3cf1b224efde55fb4e562657eba4"
+      url "https://github.com/nclandrei/proctor/releases/download/v0.1.1/proctor-aarch64-apple-darwin.tar.gz"
+      sha256 "bbf14c837328b2345b8451862a28f38271ef50ca5d8e68c144ce28a0e789b5a3"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/nclandrei/homebrew-tap/releases/download/proctor-v0.1.0/proctor-x86_64-apple-darwin.tar.gz"
-      sha256 "0932b97b05613656dc61ac02b0bdb1d4fe11c21985dddd896f398e221c40eee7"
+      url "https://github.com/nclandrei/proctor/releases/download/v0.1.1/proctor-x86_64-apple-darwin.tar.gz"
+      sha256 "692cc5f961a4fcd823c89016251e67285e1d08234c1e6c466e0ac02617453c81"
     end
   end
   if OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/nclandrei/homebrew-tap/releases/download/proctor-v0.1.0/proctor-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "6c2c1d2cddb16a8677a6c89a26a92fc614345b59b2e01f0bc232f35755531851"
+      url "https://github.com/nclandrei/proctor/releases/download/v0.1.1/proctor-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "9a71d080835e4216a614686881db3e7bb69011746f51941cee2a0713a8125aed"
     end
   end
 
