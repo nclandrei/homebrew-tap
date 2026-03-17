922b4897630f1c8a48fb90b59c8bc66da0059a68
Andrei Nicolae <actions@users.noreply.github.com>
2026-03-17 08:59:26 +0200

    magellan 0.1.3

--- Formula/magellan.rb
+++ Formula/magellan.rb
@@ -1,14 +1,31 @@
 class Magellan < Formula
   desc "Deterministic presentation engine for AI-generated technical walkthroughs"
   homepage "https://github.com/nclandrei/magellan"
-  url "https://static.crates.io/crates/magellan-cli/magellan-cli-0.1.2.crate"
-  sha256 "97b0a58d0ace19a2a86e69fc478fedc81b1abf718eaaeebcaf05bbe221e07298"
+  version "0.1.3"
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/nclandrei/magellan/releases/download/v0.1.3/magellan-aarch64-apple-darwin.tar.xz"
+      sha256 "2486281a2a6bc2eb180b8b9765c9087f09ef67a3145cfb1623a437fea135a5c4"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/nclandrei/magellan/releases/download/v0.1.3/magellan-x86_64-apple-darwin.tar.xz"
+      sha256 "305399a9d3f9367c6858b68ff7f5f6eae246860c71ee7230c2154662a22171b0"
+    end
+  end
+  if OS.linux?
+    if Hardware::CPU.intel?
+      url "https://github.com/nclandrei/magellan/releases/download/v0.1.3/magellan-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "1a3b77b2212c029978a4500f6c50631f223b24d52897d60538e7a3d9be8adf91"
+    end
+  end
   license "MIT"
 
-  depends_on "rust" => :build
-
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "magellan"
+
+    doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
+    leftover_contents = Dir["*"] - doc_files - ["magellan"]
+    pkgshare.install(*leftover_contents) unless leftover_contents.empty?
   end
 
   test do
