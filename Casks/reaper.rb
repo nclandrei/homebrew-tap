910b5729c55622dee135f40cadcff78143c3a86e
Andrei-Mihai Nicolae <andrei@nicolaeandrei.com>
2026-03-24 14:30:08 +0200

    Update reaper to v0.1.0

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,6 +1,6 @@
 cask "reaper" do
-  version "0.0.0"
-  sha256 :no_check
+  version "0.1.0"
+  sha256 "2b6b6359916769c9a6e2ad19782b61b7a7de044093c56876522c5d24708c1607"
 
   url "https://github.com/nclandrei/reaper/releases/download/v#{version}/Reaper.zip"
   name "Reaper"
