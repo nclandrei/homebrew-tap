279f5a2f05a291c1094b2c48ae68e91e55ceb7d5
Andrei Nicolae <andrei@nicolaeandrei.com>
2026-03-27 13:45:31 +0000

    chore: refresh snapshot formulas (distill.rb,magellan.rb proctor.rb)

--- Formula/distill.rb
+++ Formula/distill.rb
@@ -1,65 +1,18 @@
 class Distill < Formula
-  desc "CLI tool that monitors AI agent sessions, identifies patterns, and proposes skills"
+  desc "Monitor AI agent sessions and propose reusable skills"
   homepage "https://github.com/nclandrei/distill"
-  version "0.5.4"
-  if OS.mac?
-    if Hardware::CPU.arm?
-      url "https://github.com/nclandrei/distill/releases/download/v0.5.4/distill-cli-aarch64-apple-darwin.tar.xz"
-      sha256 "abaeef636bed9441cda70c085c37229ec1851c5a27ae47c41cdf697de1af1bee"
-    end
-    if Hardware::CPU.intel?
-      url "https://github.com/nclandrei/distill/releases/download/v0.5.4/distill-cli-x86_64-apple-darwin.tar.xz"
-      sha256 "5c0060b33f86852621f330307d2b1eea9fff307df8b8927d5fd63ed44b12fe1c"
-    end
-  end
-  if OS.linux?
-    if Hardware::CPU.intel?
-      url "https://github.com/nclandrei/distill/releases/download/v0.5.4/distill-cli-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "98ae44b5c656b45b73d4ab7f43ee5a7c6417fee2b36dda1da647d753e2d6a53a"
-    end
-  end
+  url "https://github.com/nclandrei/distill/archive/6c10b7cf688c3ddcfa7b577229a3d0e35314f111.tar.gz"
+  version "0.5.4+git.20260327.6c10b7c"
+  sha256 "5fb0380cec9c1477d5f985fb4f66992ba91024e01970eff24a24af85754c8bf9"
   license "MIT"
 
-  BINARY_ALIASES = {
-    "aarch64-apple-darwin": {},
-    "x86_64-apple-darwin": {},
-    "x86_64-unknown-linux-gnu": {}
-  }
-
-  def target_triple
-    cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
-    os = OS.mac? ? "apple-darwin" : "unknown-linux-gnu"
-
-    "#{cpu}-#{os}"
-  end
-
-  def install_binary_aliases!
-    BINARY_ALIASES[target_triple.to_sym].each do |source, dests|
-      dests.each do |dest|
-        bin.install_symlink bin/source.to_s => dest
-      end
-    end
-  end
+  depends_on "rust" => :build
 
   def install
-    if OS.mac? && Hardware::CPU.arm?
-      bin.install "distill"
-    end
-    if OS.mac? && Hardware::CPU.intel?
-      bin.install "distill"
-    end
-    if OS.linux? && Hardware::CPU.intel?
-      bin.install "distill"
-    end
+    system "cargo", "install", *std_cargo_args
+  end
 
-    install_binary_aliases!
-
-    # Homebrew will automatically install these, so we don't need to do that
-    doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
-    leftover_contents = Dir["*"] - doc_files
-
-    # Install any leftover files in pkgshare; these are probably config or
-    # sample files.
-    pkgshare.install(*leftover_contents) unless leftover_contents.empty?
+  test do
+    assert_match "distill", shell_output("#{bin}/distill --help")
   end
 end
--- Formula/magellan.rb
+++ Formula/magellan.rb
@@ -1,31 +1,15 @@
 class Magellan < Formula
   desc "Deterministic presentation engine for AI-generated technical walkthroughs"
   homepage "https://github.com/nclandrei/magellan"
-  version "0.2.0"
-  if OS.mac?
-    if Hardware::CPU.arm?
-      url "https://github.com/nclandrei/magellan/releases/download/v0.2.0/magellan-aarch64-apple-darwin.tar.xz"
-      sha256 "8b1329f2318f279f68f2343051706d8c9eee780d9736ffb7a635705e138cdfce"
-    end
-    if Hardware::CPU.intel?
-      url "https://github.com/nclandrei/magellan/releases/download/v0.2.0/magellan-x86_64-apple-darwin.tar.xz"
-      sha256 "18e10e513f125e84a793b3998ac21a2fd8e52189eba05d1914b4c54ab5756079"
-    end
-  end
-  if OS.linux?
-    if Hardware::CPU.intel?
-      url "https://github.com/nclandrei/magellan/releases/download/v0.2.0/magellan-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "4492a8b6e2dacf93955d29ef3c4be23c0b61b62d5da8e83c516c1ca47e81a444"
-    end
-  end
+  url "https://github.com/nclandrei/magellan/archive/8dde0c88b5f2579bc9e6ff8cf694c658b42ebdc3.tar.gz"
+  version "0.2.0+git.20260327.8dde0c8"
+  sha256 "1e637c18547ef6b6d15578632ab69011330c170b474656ef93a87d44c8b37a08"
   license "MIT"
 
-  def install
-    bin.install "magellan"
+  depends_on "rust" => :build
 
-    doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
-    leftover_contents = Dir["*"] - doc_files - ["magellan"]
-    pkgshare.install(*leftover_contents) unless leftover_contents.empty?
+  def install
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
