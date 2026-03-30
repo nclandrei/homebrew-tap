d7a414e875a73ef780a97679cffe7e617e9e8aee
Andrei Nicolae <andrei@nicolaeandrei.com>
2026-03-30 08:02:41 +0000

    chore: refresh snapshot formulas (distill.rb)

--- Formula/distill.rb
+++ Formula/distill.rb
@@ -1,9 +1,9 @@
 class Distill < Formula
   desc "Monitor AI agent sessions and propose reusable skills"
   homepage "https://github.com/nclandrei/distill"
-  url "https://github.com/nclandrei/distill/archive/6c10b7cf688c3ddcfa7b577229a3d0e35314f111.tar.gz"
-  version "0.5.4+git.20260327.6c10b7c"
-  sha256 "5fb0380cec9c1477d5f985fb4f66992ba91024e01970eff24a24af85754c8bf9"
+  url "https://github.com/nclandrei/distill/archive/3703a62578d724b3729d59f1b0c6ada4d30e4e05.tar.gz"
+  version "0.5.4+git.20260330.3703a62"
+  sha256 "27e237cd34ec077206f3580cada851f5e82ea733eea2867049e658c0af52202a"
   license "MIT"
 
   depends_on "rust" => :build
