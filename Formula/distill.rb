3e1efa35f6afd5736ed2ec3844a9dc6903e28355
Andrei Nicolae <andrei@nicolaeandrei.com>
2026-04-02 13:58:16 +0000

    chore: refresh snapshot formulas (distill.rb)

--- Formula/distill.rb
+++ Formula/distill.rb
@@ -1,65 +1,18 @@
 class Distill < Formula
-  desc "CLI tool that monitors AI agent sessions, identifies patterns, and proposes skills"
+  desc "Monitor AI agent sessions and propose reusable skills"
   homepage "https://github.com/nclandrei/distill"
-  version "0.6.4"
-  if OS.mac?
-    if Hardware::CPU.arm?
-      url "https://github.com/nclandrei/distill/releases/download/v0.6.4/distill-cli-aarch64-apple-darwin.tar.xz"
-      sha256 "601f36bfdbbfa7d5ab782ea1c87ec5b03f322e8dbb8bb4778afbdf86ab33522e"
-    end
-    if Hardware::CPU.intel?
-      url "https://github.com/nclandrei/distill/releases/download/v0.6.4/distill-cli-x86_64-apple-darwin.tar.xz"
-      sha256 "b1489860327e0941a8afa91a02c667a747a475d250d3c671b6d23619e88b63e7"
-    end
-  end
-  if OS.linux?
-    if Hardware::CPU.intel?
-      url "https://github.com/nclandrei/distill/releases/download/v0.6.4/distill-cli-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "8ce648ce35fc6316beead5019d1e5ac18a88254ef039634a74631e0d2fd1ef54"
-    end
-  end
+  url "https://github.com/nclandrei/distill/archive/bd8f6482b37d70d407d81aeb8925f4b833d398a8.tar.gz"
+  version "0.6.4+git.20260402.bd8f648"
+  sha256 "e39c009aa7fd7d859a05d1f23832e23d032af965ca4160a0f0d284dfcd20d752"
   license "MIT"
 
-  BINARY_ALIASES = {
-    "aarch64-apple-darwin": {},
-    "x86_64-apple-darwin": {},
-    "x86_64-unknown-linux-gnu": {}
-  }
-
-  def target_triple
-    cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
-    os = OS.mac? ? "apple-darwin" : "unknown-linux-gnu"
-
-    "#{cpu}-#{os}"
-  end
-
-  def install_binary_aliases!
-    BINARY_ALIASES[target_triple.to_sym].each do |source, dests|
-      dests.each do |dest|
-        bin.install_symlink bin/source.to_s => dest
-      end
-    end
-  end
+  depends_on "rust" => :build
 
   def install
-    if OS.mac? && Hardware::CPU.arm?
-      bin.install "distill"
-    end
-    if OS.mac? && Hardware::CPU.intel?
-      bin.install "distill"
-    end
-    if OS.linux? && Hardware::CPU.intel?
-      bin.install "distill"
-    end
+    system "cargo", "install", *std_cargo_args
+  end
 
-    install_binary_aliases!
-
-    # Homebrew will automatically install these, so we don't need to do that
-    doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
-    leftover_contents = Dir["*"] - doc_files
-
-    # Install any leftover files in pkgshare; these are probably config or
-    # sample files.
-    pkgshare.install(*leftover_contents) unless leftover_contents.empty?
+  test do
+    assert_match "distill", shell_output("#{bin}/distill --help")
   end
 end
